548ccbabf0fe5397b1d4eb4edff9787a304fc24a
Amirerfan <amirerfan@pop-os.localdomain>
2023-05-10 00:00:39 +0200

    Fix Landing Header Wave

--- src/pages/landing/components/header.tsx
+++ src/pages/landing/components/header.tsx
@@ -8,7 +8,7 @@ const Header = () => {
 		<section
 			id="home-header"
 			className={
-				'uni-card flex flex-col md:flex-row gap-4 after:rounded-2xl after:bg-home-header-texture h-52 text-white justify-center text-center md:text-left md:p-4 md:pl-6 overflow-hidden'
+				'landing__header flex flex-col md:flex-row after:rounded-2xl bg-gray20 rounded-2xl h-52 text-white justify-center text-center md:text-left md:p-4 md:pl-6 overflow-hidden'
 			}
 		>
 			{userProfile && <span className="home-header__left flex-1 flex flex-row gap-5">
@@ -18,8 +18,8 @@ const Header = () => {
 					<p className='level text-xs text-gray90'>Level: [Coming soon]</p>
 				</span>
 			</span>}
-			<span className="home-header__right flex-1 text-right">
-				<img src='/assets/images/landing/uni-logo.svg' className={'w-40 ml-auto mb-3 mr-5'} alt={'logo'}/>
+			<span className={`home-header__right flex-1 flex flex-col justify-start items-end ${!userProfile && 'justify-center items-center'}`}>
+				<img src='/assets/images/landing/uni-logo.svg' className={'w-40 mb-3 mr-5'} alt={'logo'}/>
 				<h4 className='text-text-secondary font-bold'>
 					The gateway to web3.
 				</h4>
