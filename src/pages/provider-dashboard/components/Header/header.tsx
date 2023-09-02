86bbed21fe0997f0d2c433666e08aac8cf256fbf
Ali Maktabi <alimaktabi55@yahoo.com>
2023-09-02 09:48:37 +0330

    removed extra stylings

--- src/pages/provider-dashboard/components/Header/header.tsx
+++ src/pages/provider-dashboard/components/Header/header.tsx
@@ -1,6 +1,6 @@
 const Header = () => {
 	return (
-		<div className="header providerDashboard__header w-full h-[202px] rounded-2xl flex items-center overflow-hidden p-5 mb-6  ">
+		<div className="header providerDashboard__header w-full h-[202px] rounded-2xl flex items-center overflow-hidden p-5 mb-6">
 			<div className="header__left items-center h-auto">
 				<img className="gas-tap h-auto w-375 mb-3" src="assets/images/provider-dashboard/blibk neon.png" />
 				<p className="z-10 text-sm text-gray100 text-[12px] max-w-[365px]">
